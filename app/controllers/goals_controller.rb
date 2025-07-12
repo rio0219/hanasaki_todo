a9e3c2bbbc4260c7acc20c19a6ef78ca1f293393
久保田莉央 <rio@kubotariounoMacBook-Air.local>
2025-07-12 23:53:42 +0900

    記録できる様にした!

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,16 +1,31 @@
 class GoalsController < ApplicationController
   def record
-    puts "★★ recordアクションが呼ばれました"
-    puts "★★ params[:id] = #{params[:id].inspect}"
-
     @goal = Goal.includes(:tasks).find(params[:id])
-    puts "★★ @goal = #{@goal.inspect}"
-
     @today = Date.current
+  
     @records = @goal.tasks.map do |task|
       TaskRecord.find_or_initialize_by(task: task, date: @today)
     end
   end
+  
+  def record_submit
+    goal = Goal.find(params[:id])
+    today = Date.current
+  
+    records_params = params[:records] || []
+    records_params.each do |record_param|
+      task_id = record_param[:task_id]
+      done = record_param[:done] == "1"
+  
+      task = Task.find(task_id)
+      task_record = TaskRecord.find_or_initialize_by(task: task, date: today)
+      task_record.done = done
+      task_record.save!
+    end
+  
+    redirect_to goals_path, notice: "記録を保存しました"
+  end
+ 
   def new
     @goal = Goal.new
     5.times { @goal.tasks.build }
@@ -52,20 +67,6 @@ class GoalsController < ApplicationController
 
   def goal_params
     params.require(:goal).permit(:title, tasks_attributes: [:id, :title, :_destroy])
-  end
-  # 記録ページを表示
-  
-  
-
-# 記録を保存
-  def record_submit
-    params[:records]&.each do |record_params|
-      record = TaskRecord.find_or_initialize_by(task_id: record_params[:task_id], date: Date.current)
-      record.done = record_params[:done] == "1"
-      record.save!
-    end
-
-    redirect_to goals_path, notice: "記録を保存しました"
   end
 end
 
